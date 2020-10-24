dcbb4e8472e7a0afe7d505c5ec83982afdd666d3
Lucas <lopez.lucas@hotmail.com>
2020-10-24 22:57:22 +0200

    Connexion à la db

---
+++ MyMoneyManagerBackend/Infrastructure/SqlServer/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Infrastructure.SqlServer
+{
+    public static class Database
+    {
+        private static readonly string ConnectionString = "Server=.\\SQLExpress;Database=todos;Integrated Security=SSPI";
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
